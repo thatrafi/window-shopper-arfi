91ef7e2b35cbb70492e4f3230f95d84c7cca62b7
that_rafi <muhammad.rafiudin@students.amikom.ac.id>
2020-07-29 16:20:59 +0700

    Initial Commit

---
+++ window-shopper-arfi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper-arfi
+//
+//  Created by rafiudin on 29/07/20.
+//  Copyright © 2020 rafiudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
